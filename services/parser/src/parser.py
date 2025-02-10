3e8a3ca99c2b277b7b1a7a547776feae9d35d0f4
Amin Ehsan <Mr.AminEhsan@gmail.com>
2025-02-11 01:18:23 +0330

    Update parser.py

--- services/parser/src/parser.py
+++ services/parser/src/parser.py
@@ -1,4 +1,9 @@
 class Parser:
     @staticmethod
     def pars_ads_list(ads_list: list) -> list:
-        return ads_list
+        ads_pars_list = list()
+        for ads in ads_list:
+            for widget in ads['list_widgets']:
+                if widget['widget_type'] == 'POST_ROW':
+                    ads_pars_list.append(widget)
+        return ads_pars_list
